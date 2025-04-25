2621afcf374d186d59ed31cd9529b70901578baf
bin <vodinhquan2707.it@gmail.com>
2025-04-26 01:20:23 +0700

    Time: 45 ms (63.51%), Space: 54.1 MB (91.21%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,23 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let called: boolean = false;
+    let result: any;
+    return function (...args) {
+        if (!called) {
+            called = true;
+            result = fn(...args);
+            return result;
+        }
+        return undefined;
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
