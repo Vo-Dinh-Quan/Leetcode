95d5ce05630b09bb1bc0c80b548e463b996d12c7
bin <vodinhquan2707.it@gmail.com>
2025-05-07 11:49:57 +0700

    Time: 63 ms (28.21%), Space: 55.3 MB (77.68%) - LeetHub

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.ts
@@ -0,0 +1,37 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+    const timer = setTimeout(() => { // ở đây chỉ đơn giản là lưu cái timer ID vào biến timer để sau này clearTimeout nó nếu cần thôi. sau khi gọi cancellable thì cái setTimeout này vẫn chạy bình thường
+        fn(...args);
+    }, t);
+
+    const cancelFn = () => {
+        clearTimeout(timer);
+    }
+    return cancelFn;
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
